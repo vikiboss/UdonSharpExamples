635449a574725a0f00b284e229cef0b9ea659c2d
Viki <hi@viki.moe>
2025-05-29 23:35:33 +0800

    chore: modify slider counter

--- 05_SliderCounter/SliderCounter.cs
+++ 05_SliderCounter/SliderCounter.cs
@@ -2,6 +2,7 @@ using TMPro;
 using UdonSharp;
 using UnityEngine;
 using UnityEngine.UI;
+using VRC.SDK3.UdonNetworkCalling;
 using VRC.SDKBase;
 using VRC.Udon.Common.Interfaces;
 
@@ -11,7 +12,8 @@ public class SliderCounter : UdonSharpBehaviour {
 
   private Slider _slider;
   private const float SYNC_THRESHOLD = 0.01f;
-
+  private const float SYNC_THROTTLE = 0.1f;
+  private float lastChangeAt = 0;
 
   [UdonSynced, FieldChangeCallback(nameof(sliderValue))]
   private float _sliderValue = 0f;
@@ -30,15 +32,19 @@ public class SliderCounter : UdonSharpBehaviour {
   }
 
   public void HandleValueChangedBySlider() {
+    if (Time.time != 0 && Time.time - lastChangeAt < SYNC_THROTTLE) return;
     if (Mathf.Abs(_slider.value - _sliderValue) <= SYNC_THRESHOLD) return;
+
+    lastChangeAt = Time.time;
 
     if (Networking.IsOwner(gameObject)) {
       OnValueChanged(_slider.value);
     } else {
-      SendCustomNetworkEvent(NetworkEventTarget.Owner, nameof(OnValueChanged), _slider.value);
+      SendCustomNetworkEvent(NetworkEventTarget.Owner, "OnValueChanged", _slider.value);
     }
   }
 
+  [NetworkCallable]
   public void OnValueChanged(float nextValue) {
     if (Networking.IsOwner(gameObject)) {
       sliderValue = nextValue;
